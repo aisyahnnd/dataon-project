ff931a8ee581b64f410fffac59455e2261dbaa3c
aisyahnnd <aisyah214.ank@gmail.com>
2022-07-28 13:17:25 +0700

    bugfix: styling-filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { MyTrainingCard } from './Parts/MyTrainingCard/MyTrainingCard';
 import { AllTrainingEvent } from './Parts/AllTrainingEvent/AllTrainingEvent';
+import FilterTrainingEvent from './Parts/FilterTrainingEvent';
 import './App.css';
 
 function App() {
   return (
     <>
+      <FilterTrainingEvent />
       <MyTrainingCard />
       <AllTrainingEvent />
     </>
