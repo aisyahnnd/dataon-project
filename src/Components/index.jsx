0f918eecfc9230ef953217a314424ace7527bfee
Fatur Rahman <faturrahmanavoiders@gmail.com>
2022-07-29 13:54:18 +0700

    feat: create table all event & and link to location

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -3,4 +3,5 @@ import SelectBox from './Select'
 import ButtonIcon from './ButtonIcon'
 import Toggle from './Toogle'
 import CardEvent from './CardEvent'
-export { TextInput, SelectBox, ButtonIcon, Toggle, CardEvent }
+import Link from './Link'
+export { TextInput, SelectBox, ButtonIcon, Toggle, CardEvent, Link }
