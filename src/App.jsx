5710195fbfca26140c7832d62d413aaab55344a4
aisyahnnd <aisyah214.ank@gmail.com>
2022-08-02 14:50:13 +0700

    feat: create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { MyTrainingCard } from "./Parts/MyTrainingCard/MyTrainingCard";
-import { AllTrainingEvent } from "./Parts/AllTrainingEvent/AllTrainingEvent";
-import FilterTrainingEvent from "./Parts/FilterTrainingEvent";
-import { AllTrainingEventTable, MyTrainingEventTable } from "./Parts";
-import "./App.css";
-import { SectionHeader } from "./Parts/SectionHeader/SectionHeader";
-import { TrainingEditPage } from "./Pages/TrainingEditPage/TrainingEditPage";
-import { TrainingDetailPage } from "./Pages/TrainingDetailPage/TrainingDetailPage";
-import { TrainingCreatePage } from "./Pages/TrainingCreatePage /TrainingCreatePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MissingPage } from "./Pages/MissingPage/MissingPage";
+import { MyTrainingCard } from './Parts/MyTrainingCard/MyTrainingCard';
+import { AllTrainingEvent } from './Parts/AllTrainingEvent/AllTrainingEvent';
+import FilterTrainingEvent from './Parts/FilterTrainingEvent';
+import { AllTrainingEventTable, MyTrainingEventTable } from './Parts';
+import './App.css';
+import { SectionHeader } from './Parts/SectionHeader/SectionHeader';
+import { TrainingEditPage } from './Pages/TrainingEditPage/TrainingEditPage';
+import { TrainingDetailPage } from './Pages/TrainingDetailPage/TrainingDetailPage';
+import { TrainingCreatePage } from './Pages/TrainingCreatePage /TrainingCreatePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MissingPage } from './Pages/MissingPage/MissingPage';
+import { LoginPage } from './Pages/LoginPage/LoginPage';
+import { RegisterPage } from './Pages/RegisterPage/RegisterPage';
 
 const App = () => {
   return (
@@ -27,12 +29,26 @@ const App = () => {
             </>
           }
         />
-        <Route path="/training/:id" element={<TrainingDetailPage />} />
-        <Route path="/training/create" element={<TrainingCreatePage />} />
-        <Route path="/mytraining/:id" element={<TrainingDetailPage />} />
-        <Route path="/mytraining/edit/:id" element={<TrainingEditPage />} />
+        <Route
+          path="/training/:id"
+          element={<TrainingDetailPage />}
+        />
+        <Route
+          path="/training/create"
+          element={<TrainingCreatePage />}
+        />
+        <Route
+          path="/mytraining/:id"
+          element={<TrainingDetailPage />}
+        />
+        <Route
+          path="/mytraining/edit/:id"
+          element={<TrainingEditPage />}
+        />
         <Route path="*" exact={true} element={<MissingPage />} />
         <Route path="/missing" element={<MissingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,318 @@
+import {
+  Card,
+  Col,
+  Row,
+  Space,
+  Typography,
+  Dropdown,
+  Menu,
+  Button,
+  Form,
+  Input,
+  Carousel,
+  Alert,
+} from 'antd';
+import './RegisterPage.css';
+import { DownOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const menu = (
+  <Menu
+    items={[
+      {
+        type: 'divider',
+      },
+      {
+        label: 'English (EN)',
+        key: '1',
+      },
+      {
+        label: 'Indonesia (IDN)',
+        key: '2',
+      },
+    ]}
+  />
+);
+
+const { Text } = Typography;
+
+export const RegisterPage = () => {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [flag, setFlag] = useState(false);
+
+  let navigate = useNavigate();
+
+  const onFinish = (values) => {
+    if (!name || !username || !password) {
+      setFlag(true);
+    } else {
+      setFlag(false);
+      localStorage.setItem('Username', JSON.stringify(username));
+      localStorage.setItem('Password', JSON.stringify(password));
+
+      console.log('Save in localstorage');
+
+      alert('Registration Success');
+      navigate('/login');
+    }
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <>
+      <Card bodyStyle={{ backgroundColor: '#bac6f2' }}>
+        <Card
+          bodyStyle={{
+            padding: '0px',
+          }}
+          style={{ borderRadius: 10 }}
+        >
+          <Row
+            className="header"
+            style={{ borderBottom: '1px #dddddd solid' }}
+          >
+            <Col span={3}>
+              <img alt="logo" src="logo.png" width={150} />
+            </Col>
+            <Col
+              span={18}
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: 10,
+              }}
+            >
+              <Space size={-5} direction="vertical">
+                <Text style={{ fontSize: '16px', fontWeight: 500 }}>
+                  Human Resources Information System
+                </Text>
+                <Text style={{ fontSize: '24px', fontWeight: 900 }}>
+                  SunFish 7
+                </Text>
+              </Space>
+            </Col>
+            <Col
+              span={3}
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                paddingTop: 30,
+              }}
+            >
+              <Dropdown overlay={menu} trigger={['click']}>
+                <a onClick={(e) => e.preventDefault()}>
+                  <Space>
+                    <Text style={{ color: 'black' }}>
+                      English (EN)
+                    </Text>
+                    <DownOutlined style={{ color: 'black' }} />
+                  </Space>
+                </a>
+              </Dropdown>
+            </Col>
+          </Row>
+          <Row className="content">
+            <Col span={14}>
+              <Carousel
+                style={{
+                  justifyContent: 'center',
+                  paddingTop: 100,
+                  paddingLeft: 100,
+                }}
+                autoplay
+              >
+                <div>
+                  <img
+                    style={{
+                      width: '500px',
+                      justifyContent: 'center',
+                    }}
+                    alt="side-1"
+                    src="example-3.svg"
+                  />
+                </div>
+                <div>
+                  <img
+                    style={{
+                      width: '500px',
+                      justifyContent: 'center',
+                    }}
+                    alt="side-1"
+                    src="example-25.svg"
+                  />
+                </div>
+                <div>
+                  <img
+                    style={{
+                      width: '500px',
+                      justifyContent: 'center',
+                    }}
+                    alt="side-1"
+                    src="example-29.svg"
+                  />
+                </div>
+                <div>
+                  <img
+                    style={{
+                      width: '500px',
+                      justifyContent: 'center',
+                    }}
+                    alt="side-1"
+                    src="example-30.svg"
+                  />
+                </div>
+              </Carousel>
+            </Col>
+            <Col span={10} style={{ padding: 20 }}>
+              <Space
+                size={3}
+                style={{ paddingRight: 30, paddingBottom: 30 }}
+              >
+                <Text
+                  style={{
+                    fontSize: '36px',
+                    fontWeight: 900,
+                    color: '#1890ff',
+                  }}
+                >
+                  Please enter your credential to access the system
+                </Text>
+              </Space>
+              <Form
+                name="basic"
+                wrapperCol={{
+                  span: 18,
+                }}
+                initialValues={{
+                  remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+                layout="vertical"
+              >
+                <Form.Item
+                  style={{ fontWeight: 'bold' }}
+                  label="Name"
+                  name="name"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input your name!',
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter your name here"
+                    onChange={(event) => setName(event.target.value)}
+                  />
+                </Form.Item>
+                <Form.Item
+                  style={{ fontWeight: 'bold' }}
+                  label="Username"
+                  name="username"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input your username!',
+                    },
+                    {
+                      max: 20,
+                      message: 'Username must be less than 20',
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter your username here"
+                    onChange={(event) =>
+                      setUsername(event.target.value)
+                    }
+                  />
+                </Form.Item>
+                <Form.Item
+                  style={{ fontWeight: 'bold' }}
+                  label="Password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input your password!',
+                    },
+                    {
+                      min: 8,
+                      message:
+                        'Passwords must be at least 8 characters',
+                    },
+                  ]}
+                >
+                  <Input.Password
+                    placeholder="Password"
+                    onChange={(event) =>
+                      setPassword(event.target.value)
+                    }
+                  />
+                </Form.Item>
+                <Form.Item
+                  wrapperCol={{
+                    offset: 0,
+                    span: 24,
+                  }}
+                >
+                  <Row>
+                    <Col span={24}>
+                      <p>
+                        Have an account?{' '}
+                        <Link to="/login">Login</Link>
+                      </p>
+                    </Col>
+                  </Row>
+                  <Button
+                    style={{ width: 100 }}
+                    type="primary"
+                    htmlType="submit"
+                  >
+                    Register
+                  </Button>
+                </Form.Item>
+
+                {flag && (
+                  <Alert
+                    message="Please fill every field!"
+                    type="warning"
+                  />
+                )}
+              </Form>
+            </Col>
+          </Row>
+          <Row className="footer">
+            <Col
+              span={24}
+              style={{ textAlign: 'center', paddingTop: 20 }}
+            >
+              <Text style={{ fontSize: '16px', color: '#888888' }}>
+                This product is licensed for Dataon Corporation
+              </Text>
+            </Col>
+            <Col
+              span={24}
+              style={{ textAlign: 'center', paddingBottom: 20 }}
+            >
+              <Text style={{ fontSize: '16px', color: '#888888' }}>
+                &copy; 1999 - 2022 DataOn Technology. All Rights
+                Reserved
+              </Text>
+            </Col>
+          </Row>
+        </Card>
+      </Card>
+    </>
+  );
+};
