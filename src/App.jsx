8d4dbaed46718bcc12b07eb2dd0833b3369bcb4f
aisyahnnd <aisyah214.ank@gmail.com>
2022-08-03 15:11:36 +0700

    feat: integration login

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,52 @@
-import "./App.css";
-import { TrainingEditPage } from "./Pages/TrainingEditPage/TrainingEditPage";
-import { TrainingDetailPage } from "./Pages/TrainingDetailPage/TrainingDetailPage";
-import { TrainingCreatePage } from "./Pages/TrainingCreatePage /TrainingCreatePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MissingPage } from "./Pages/MissingPage/MissingPage";
-import { LoginPage } from "./Pages/LoginPage/LoginPage";
-import { RegisterPage } from "./Pages/RegisterPage/RegisterPage";
-import Dashboard from "./Pages/Dashboard";
+import './App.css';
+import { TrainingEditPage } from './Pages/TrainingEditPage/TrainingEditPage';
+import { TrainingDetailPage } from './Pages/TrainingDetailPage/TrainingDetailPage';
+import { TrainingCreatePage } from './Pages/TrainingCreatePage /TrainingCreatePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MissingPage } from './Pages/MissingPage/MissingPage';
+import { LoginPage } from './Pages/LoginPage/LoginPage';
+import { RegisterPage } from './Pages/RegisterPage/RegisterPage';
+import Dashboard from './Pages/Dashboard';
+
+const setToken = (userToken) => {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+};
+
+const getToken = () => {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken;
+};
 
 const App = () => {
+  const token = getToken();
+
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route
-          path="/"
-          element={
-            <>
-              <Dashboard></Dashboard>
-            </>
-          }
+          path="/training/:id"
+          element={<TrainingDetailPage />}
         />
-        <Route path="/training/:id" element={<TrainingDetailPage />} />
-        <Route path="/training/create" element={<TrainingCreatePage />} />
-        <Route path="/mytraining/:id" element={<TrainingDetailPage />} />
-        <Route path="/mytraining/edit/:id" element={<TrainingEditPage />} />
+        <Route
+          path="/training/create"
+          element={<TrainingCreatePage />}
+        />
+        <Route
+          path="/mytraining/:id"
+          element={<TrainingDetailPage />}
+        />
+        <Route
+          path="/mytraining/edit/:id"
+          element={<TrainingEditPage />}
+        />
         <Route path="*" exact={true} element={<MissingPage />} />
         <Route path="/missing" element={<MissingPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/login"
+          element={<LoginPage setToken={setToken} />}
+        />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
--- src/Pages/RegisterPage/RegisterPage.jsx
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -4,7 +4,6 @@ import {
   Row,
   Space,
   Typography,
-  Menu,
   Button,
   Form,
   Input,
@@ -21,24 +20,6 @@ import Image2 from '../../assets/Images/example-25.svg';
 import Image3 from '../../assets/Images/example-29.svg';
 import Image4 from '../../assets/Images/example-30.svg';
 import Logo from '../../assets/Images/logo.png';
-
-const menu = (
-  <Menu
-    items={[
-      {
-        type: 'divider',
-      },
-      {
-        label: 'English (EN)',
-        key: '1',
-      },
-      {
-        label: 'Indonesia (IDN)',
-        key: '2',
-      },
-    ]}
-  />
-);
 
 const { Text } = Typography;
 
@@ -193,7 +174,7 @@ export const RegisterPage = () => {
               <Form
                 name="basic"
                 wrapperCol={{
-                  span: 18,
+                  span: 24,
                 }}
                 initialValues={{
                   remember: true,
@@ -215,6 +196,7 @@ export const RegisterPage = () => {
                   ]}
                 >
                   <Input
+                    style={{ width: 400 }}
                     placeholder="Enter your name here"
                     onChange={(event) => setName(event.target.value)}
                   />
@@ -240,6 +222,7 @@ export const RegisterPage = () => {
                   ]}
                 >
                   <Input
+                    style={{ width: 400 }}
                     placeholder="Enter your username here"
                     onChange={(event) =>
                       setUsername(event.target.value)
@@ -263,6 +246,7 @@ export const RegisterPage = () => {
                   ]}
                 >
                   <Input.Password
+                    style={{ width: 400 }}
                     placeholder="Password"
                     onChange={(event) =>
                       setPassword(event.target.value)
