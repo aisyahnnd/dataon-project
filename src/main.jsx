e53ecd8ab43a8dd1e4559599708cc9c210a61edf
aisyahnnd <aisyah214.ank@gmail.com>
2022-08-10 09:37:33 +0700

    fix: configure app using environment variable

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,20 @@ import "./index.css";
 import "antd/dist/antd.css";
 import { ContextWrapper } from "./Context";
 
+const express = require("express");
+const app = express;
+require("dotenv").config();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextWrapper>
