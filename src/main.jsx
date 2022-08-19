938e768ea8f214d82a247f17a1de9c3cca801d57
aisyahnnd <aisyah214.ank@gmail.com>
2022-08-19 12:21:20 +0700

    feat: integration sentry

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,18 @@ import "./index.css";
 import "antd/dist/antd.css";
 import { ContextWrapper } from "@/Context";
 import "@/Utils/Libraries/i18n";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://be0c199315614344af962b77cc782fae@o1365819.ingest.sentry.io/6667472",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
